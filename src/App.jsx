d6d1bdbd37de76bd3d41ee34559c2705b63b6983
JavierLlanos6 <xavi6poop@gmail.com>
2023-03-28 18:02:44 -0400

    avances apartado de formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Container from "./components/Container"
-import Navbar from "./components/Navbar"
-import SideBar from "./components/Sidebar"
-function App(){
-  return (<>
-      <Navbar/>
+import Container from "./components/Container";
+import Navbar from "./components/Navbar";
+import SideBar from "./components/Sidebar";
+function App() {
+  return (
+    <>
+      <Navbar />
       <div className="d-flex">
-      <SideBar/>
-      <Container/>
+        <SideBar />
+        <Container />
       </div>
-  </>)
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Gestionar from "./components/Plazas/Gestionar";
-import Plazas from "./components/Plazas/Plazas";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Plazas></Plazas>
-    <Gestionar></Gestionar>
   </React.StrictMode>
 );
