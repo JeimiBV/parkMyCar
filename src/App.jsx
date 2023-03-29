c79f91f8b0d82532a6606a197e07f9d668428f44
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-03-28 21:52:46 -0400

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Layout from "./components/Layout"
 function App(){
   return (<>
         <Layout>
-      
+        {
+          //su página debe estar en la carpeta page y lo incluyen acá
+        }
         </Layout>
 
   </>)
