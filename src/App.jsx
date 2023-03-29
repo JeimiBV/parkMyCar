5c2b7a01c87298e6df24c5f721d20aa782b13e82
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-03-28 21:40:35 -0400

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Container from "./components/Container"
-import Navbar from "./components/Navbar"
-import SideBar from "./components/Sidebar"
+import Layout from "./components/Layout"
+
 function App(){
   return (<>
-      <Navbar/>
-      <div className="d-flex">
-      <SideBar/>
-      <Container/>
-      </div>
+        <Layout>
+      
+        </Layout>
+
   </>)
 }
 
