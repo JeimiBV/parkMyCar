8b131a3e400274ca651441aec45a905fe5a57c3e
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2023-03-31 17:02:03 -0400

    Tabla dinámica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
 
-export default function Card({children}) {
+import "../styles/Button.css"
+export default function Card({children, volverButton}) {
     return(
-        <div className="btn btn-primary m-2 d-flex justify-content-center align-items-center" >
+        <div className={volverButton?"btn volver d-flex flex-column":"btn btn-primary m-2 d-flex justify-content-center align-items-center"} >
             {children}
         </div>
     );
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,9 @@
 import "../styles/Table.css"
-export default function Table({ datos }) {
+export default function Table({ datos, fecha }) {
     return (
-        <div className="ms-5 ps-5">
+        <div className="ms-5 ps-5 mb-5">
             <section className="fs-4 fecha p-3 rounded-4 text-center mb-4">
-                Fecha
+                {fecha}
             </section>
             <div>
                 <section className="tablaContenedor w-75 rounded-4">
@@ -11,14 +11,17 @@ export default function Table({ datos }) {
                         <h4 className="col text-center"> Hora </h4>
                         <h4 className="col text-center"> Nombre </h4>
                         <h4 className="col text-center"> Placa </h4>
-                    
+
                     </div>
                     {
-                        datos.map(dato => (<div className="row p-3">
-                            <p className="col text-center">{dato.hora}</p>
-                            <p className="col text-center">{dato.nombre}</p>
-                            <p className="col text-center">{dato.placa}</p>
-                        </div>))
+                        
+                        datos.map(dato => 
+                            <div className="row p-3">
+                                <p className="col text-center">{dato.hora}</p>
+                                <p className="col text-center">{dato.nombre}</p>
+                                <p className="col text-center">{dato.placa}</p>
+                            </div>
+                        )
                     }
                 </section>
             </div>
