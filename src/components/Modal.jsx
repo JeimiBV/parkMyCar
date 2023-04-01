b2c688042fa67f89d9ba8f4a496b8073760a6465
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-04-01 18:35:52 -0400

    calendario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,8 @@
 import "../styles/Modal.css"
-export default function Modal({children}){
+export default function Modal({children, titulo}){
     return(<div className="modalContainer d-flex justify-content-center">
         <div className="modalComponent w-75">
+            <h2 className="text-center p-3">{titulo}</h2>
             {children}
         </div>
 
