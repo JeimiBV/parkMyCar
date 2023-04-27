b1a50f0fd7c05d49805498b96fa889332b77521c
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-04-26 23:48:06 -0400

    union con javier

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,43 +1,59 @@
-import "../../styles/Sidebar.css";
-import { Link } from "react-router-dom";
-export default function SideBar() {
-  return (
-    <div className="main d-lg-inline d-none pt-2 ">
-      <ul>
-        <div className="opcion">
-          <a href="/parqueo" className="text-decoration-none">
-            <div className="d-flex py-3 text-decoration-none text-light align-items-center">
-              <i class="fa-solid fa-square-parking fs-1 me-3 opciones"></i>
-              <h3 className="opciones fs-5">Plazas</h3>
-            </div>
-          </a>
-        </div>
+import "../../styles/Sidebar.css"
 
-        <div className="opcion">
-          <a href="/plazaReserva" className="text-decoration-none">
-            <div className="d-flex py-3  text-decoration-none text-light align-items-center">
-              <i class="fa-solid fa-calendar-days fs-1 me-3 opciones"></i>
-              <h3 className="opciones fs-5">Reservas</h3>
-            </div>
-          </a>
-        </div>
-        <div className="opcion">
+export default function SideBar({ cambiarE, nombreUsuario, rol }) {
+    return (
+        <div className="main d-lg-inline d-none pt-2 ">
+            <div className=" d-flex flex-column justify-content-between contenidoSide">
+                <ul>
+                    {rol == "seguridad" || "cliente" ?
+                        <div className="opcion">
+                            <a href="/parqueo" className="text-decoration-none">
+                                <div className="d-flex py-3 text-decoration-none text-light align-items-center" >
+                                    <i class="fa-solid fa-square-parking fs-1 me-3 opciones"></i>
+                                    <h3 className="opciones fs-5">Plazas</h3>
+                                </div>
+                            </a>
+                        </div> : <></>}
+
+                    {rol == "seguridad" ? <div className="opcion">
+                        <a href="/plazaReserva" className="text-decoration-none">
+                            <div className="d-flex py-3  text-decoration-none text-light align-items-center" >
+                                <i class="fa-solid fa-calendar-days fs-1 me-3 opciones"></i>
+                                <h3 className="opciones fs-5">Reservas</h3>
+                            </div>
+                        </a>
+                    </div> : <></>
+                    }
+                  {rol == "seguridad" ? <div className="opcion">
           <a href="/crearNotificacion" className="text-decoration-none">
             <div className="d-flex py-3  text-decoration-none text-light align-items-center">
               <i class="fa-solid fa-bell fs-1 me-3 opciones"></i>
               <h3 className="opciones fs-5">Notificaciones</h3>
             </div>
           </a>
-        </div>
-        <div className="opcion">
+        </div>:<></>}
+        {rol == "seguridad" ? <div className="opcion">
           <a href="/editarHorario" className="text-decoration-none">
             <div className="d-flex py-3  text-decoration-none text-light align-items-center">
               <i class="fa-solid fa-calendar-check fs-1 me-3 opciones"></i>
               <h3 className="opciones fs-5">Editar horario atencion</h3>
             </div>
           </a>
-        </div>
-      </ul>
+
+        </div>:<></>}
+                </ul>
+                <section className="text-light fs-4 px-1 py-2 logOutButton ">
+                    <div className="row py-2">
+                        <i class="fa-sharp fa-regular fa-circle-user text-light fs-1 ms-2  col-3"></i>
+                        <h4 className="col-8 ">{nombreUsuario}</h4>
+                    </div>
+
+                    <button className="btn text-light  cerrarButton" onClick={() => { cambiarE() }}>Cerrar sesión</button>
+                </section>
+            </div>
+
+
+
     </div>
   );
 }
