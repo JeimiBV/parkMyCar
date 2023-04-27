12672992a213e904f066595d6fd07afd4358b5da
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2023-04-26 22:46:44 -0400

    Notificacion-Ada

--- src/pages/CrearNotificacion.jsx
+++ src/pages/CrearNotificacion.jsx
@@ -1,196 +1,81 @@
-import React, { useState } from "react";
-import "../styles/PagesStyles/Parqueo.css";
-import { fetchData } from "../functions/fetchUsers";
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import "../styles/PagesStyles/CrearNotificacion.css";
+import axios from "axios";
 
-export default function RedactarMensaje() {
-  const [nombreDestinatarios, setNombreDestinatarios] = useState("");
-  const [descripcion, setDescripcion] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [destinatarios, setDestinatarios] = useState([]);
-  const [users, setUsers] = useState([
-    {
-      userId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      name: "Pedro",
-      email: "Pedro@gmail.com",
-    },
-    {
-      userId: "3ga85f64-5717-4562-b3fc-2c963f66afa6",
-      name: "Ana",
-      email: "Ana@gmail.com",
-    },
-    {
-      userId: "3ha85f64-5717-4562-b3fc-2c963f66afa6",
-      name: "Roberto",
-      email: "Roberto@gmail.com",
-    },
-    {
-      userId: "3ia85f64-5717-4562-b3fc-2c963f66afa6",
-      name: "Nicol",
-      email: "Nicol@gmail.com",
-    },
-    {
-      userId: "3ja85f64-5717-4562-b3fc-2c963f66afa6",
-      name: "Seiya",
-      email: "Seiya@gmail.com",
-    },
-  ]);
-
-  const handleChange = (event) => {
-    const inputValue = event.target.value.replace(/[^\w\s.,!?¿¡]/gi, "");
-    if (inputValue.length <= 200) {
-      setDescripcion(inputValue);
-    }
-  };
-
-  const handleInput = (event) => {
-    const inputValue = event.target.value;
-
-    // Filtrar los contactos que no han sido agregados como destinatarios
-    const filteredUsers = users.filter((contact) => {
-      const isAlreadySelected = destinatarios.includes(contact.email);
-      const matchesInput =
-        contact.name.toLowerCase().indexOf(inputValue.toLowerCase()) > -1 ||
-        contact.email.toLowerCase().indexOf(inputValue.toLowerCase()) > -1;
-      return !isAlreadySelected && matchesInput;
-    });
-
-    setSuggestions(filteredUsers);
-    setNombreDestinatarios(inputValue);
-  };
-
-  const getUsers = async () => {
-    const users = await fetchData();
-    setUsers(users);
-  };
-
-  const handleSelectSuggestion = (selectedSuggestion) => {
-    if (selectedSuggestion.email === "todos@example.com") {
-      const allEmails = users.map((contact) => contact.email);
-      setDestinatarios(allEmails);
-    } else if (!destinatarios.includes(selectedSuggestion.email)) {
-      setDestinatarios([...destinatarios, selectedSuggestion.email]);
-    }
-    setNombreDestinatarios("");
-    setSuggestions([]);
-  };
-
-  const handleAddDestinatario = (email) => {
-    if (!destinatarios.includes(email)) {
-      setDestinatarios([...destinatarios, email]);
-      setNombreDestinatarios("");
-      setSuggestions([]);
-    }
-  };
-
-  const handleRemoveDestinatario = (index) => {
-    const newDestinatarios = [...destinatarios];
-    newDestinatarios.splice(index, 1);
-    setDestinatarios(newDestinatarios);
-  };
+const CrearNotificacion = () => {
+  const options = [
+    { value: "Pepe", label: "68547228 " },
+    { value: "Juan", label: "75413878" },
+    { value: "Maria", label: "69712351" },
+  ];
 
   return (
-    <div>
-      <h1>Crear notificación</h1>
-      <div className="container">
-        <div className="d-flex card cardH my-4 p-4">
-          <div className="row">
-            <div className="d-flex row-1 py-2 col">
-              <p className="me-5 fs-6 m-0 w-25">Para:</p>
-              <div className="position-relative w-100">
-                <input
-                  type="text"
-                  className="w-100 h-100"
-                  value={nombreDestinatarios}
-                  onInput={handleInput}
-                  list="contacts-list"
-                />
-                <datalist id="contacts-list">
-                  {suggestions.map((contact) => (
-                    <option key={contact.email} value={contact.email}>
-                      {contact.name}
-                    </option>
-                  ))}
-                </datalist>
-                {suggestions.map((contact) => (
-                  <button
-                    key={contact.email}
-                    className="btn btn-sm btn-outline-secondary position-absolute top-0 end-0 m-1"
-                    onClick={() => handleSelectSuggestion(contact)}
-                  >
-                    +
-                  </button>
-                ))}
-              </div>
-              <div className="ms-auto">
-                <div className="dropdown">
-                  <button
-                    className="btn btn-sm btn-outline-secondary dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Todos
-                  </button>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton1"
-                  >
-                    <li>
-                      <button
-                        className="dropdown-item"
-                        onClick={() =>
-                          setDestinatarios(
-                            users.map((contact) => contact.email)
-                          )
-                        }
-                      >
-                        Seleccionar todos
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+    <div className="contenedorNot">
+      <div className="tituloNoti">Crear Notificación</div>
+      <h3 className="tituloPara">Para: </h3>
+
+      <div className="contSelect">
+        <Select className="opcionesNot" options={options} isMulti />
+      </div>
+
+      <div>
+        <h3 className="tituloDes">Descripcion: </h3>
+
+        <div className="contDes">
+          <textarea
+            className="form-control Descripcion "
+            placeholder="Escriba el mensaje"
+            id="floatingTextarea2"
+          ></textarea>
+          <div className="m-2">
+            <button type="button" className="btn btn-primary botonDes" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+              Enviar
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5 " id="staticBackdropLabel" style={{color:"black"}}>
+                Enviar Mensaje
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-          </div>
-          <div>
-            {destinatarios.map((destinatario, index) => (
-              <span
-                key={index}
-                className="badge badge-secondary m-1 position-relative"
+            <div class="modal-body">¿Estas seguro de enviar el mensaje?</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
               >
-                {destinatario}
-                <button
-                  className="btn-close position-absolute top-0 end-0 m-1"
-                  onClick={() =>
-                    setDestinatarios([
-                      ...destinatarios.slice(0, index),
-                      ...destinatarios.slice(index + 1),
-                    ])
-                  }
-                ></button>
-              </span>
-            ))}
-          </div>
-          <div>
-            <p class="me-5 fs-6 m-0 w-25">Descripción:</p>
-            <textarea
-              value={descripcion}
-              onChange={handleChange}
-              class="w-100 h-100"
-              maxLength="200"
-            ></textarea>
-          </div>
-          <div className="d-flex justify-content-end align-items-center">
-            <div
-              className="btn btn-primary m-2"
-              onClick={() => console.log(destinatarios)}
-            >
-              Publicar
+                Cerrar
+              </button>
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                Si, aceptar
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default CrearNotificacion;
