5a40324e925d8ca39500b3bc799cf60f6d10db2c
Dark-rambler <80594417+Dark-rambler@users.noreply.github.com>
2023-04-29 17:59:23 -0400

    trabajando en la autenticacion

--- src/functions/useFetch.js
+++ src/functions/useFetch.js
@@ -13,14 +13,26 @@ export function UseFetch(url){
       return data
 }
 export async function postPeticion(url,datos){
-
   await fetch(url,{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
     },
     body:JSON.stringify(datos)
-   
+  })
+  .then(res=>JSON.parse(res))
+  .then(console.log(res,"55555"))
+}
+
+export async function postAuthorization(url){
+
+  await fetch(url,{
+    method:'POST',
+    headers:{
+      'authorization':token
+    }
+  }).then((res) => res.json()).then(data => {
+    console.log(data)
   })
   
 }
--- src/protectedRoute/ProtectedRoute.jsx
+++ src/protectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import {Navigate, Outlet} from "react-router-dom"
 export const ProtectedRoute=({rol})=>{
     console.log(rol,"este es rol")
-    if(rol!="seguridad"){
+    if(rol!="admin"){
         return <Navigate to="/"/>
     }
     return <Outlet/>
