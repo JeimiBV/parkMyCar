fdc1ab17c3e3c899ff826eef177283dec5fd3b32
unknown <juliorenecg@gmail.com>
2023-05-02 21:59:59 -0400

    verQr added

--- src/pages/MisReservas.jsx
+++ src/pages/MisReservas.jsx
@@ -29,18 +29,15 @@ export default function RedactarMensaje() {
               <div>
                 <p>16:30</p>
               </div>
-              <h6 className="col"><i class="fas fa-square-parking"></i> Plaza</h6>
+              <h5 className="col"><i class="fas fa-square-parking"></i> Plaza</h5>
               <div>
                 <p>13</p>
               </div>
               
               <div className="position-relative w-100">
-                
-                
               </div>
               <div className="ms-auto">
                 <div>
-
                 </div>
               </div>
             </div>
@@ -59,24 +56,24 @@ export default function RedactarMensaje() {
         <div className="d-flex card cardH my-4 p-4">
           <div className="row">
           <h2>Informacion de la plaza</h2>
-            <div className="d-flex align-items-start mb-3 p-2 me-auto-2 py-3 row" >
-              <h4 className="me-5 fs-6 m-0 w-25"><i class="fas fa-car"></i> Parquear desde</h4>
+            <div className="my-2 col" >
+              <h4 className="col"><i class="fas fa-car"></i> Parquear desde</h4>
               <div>
                 <p>21/04/2023</p>
               </div>
-              <h4 className="me-5 fs-6 m-0 w-25"><i class="fas fa-car"></i> Parquear hasta</h4>
+              <h4 className="col"><i class="fas fa-car"></i> Parquear hasta</h4>
               <div>
                 <p>21/04/2023</p>
               </div>
-              <h5 className="me-5 fs-6 m-0 w-25"><i class="fas fa-clock"></i>  Hora Entrada</h5>
+              <h5 className="col"><i class="fas fa-clock"></i>  Hora Entrada</h5>
               <div>
                 <p>12:00</p>
               </div>
-              <h5 className="me-5 fs-6 m-0 w-25"><i class="fas fa-clock"></i>  Hora Salida</h5>
+              <h5 className="col"><i class="fas fa-clock"></i>  Hora Salida</h5>
               <div>
                 <p>16:00</p>
               </div>
-              <h6 className="me-5 fs-6 m-0 w-25"><i class="fas fa-square-parking"></i> Plaza</h6>
+              <h6 className="col"><i class="fas fa-square-parking"></i> Plaza</h6>
               <div>
                 <p>1</p>
               </div>
@@ -99,6 +96,7 @@ export default function RedactarMensaje() {
             </div>
           </div>
         </div>
+        
       </div>
     </div>
   );
