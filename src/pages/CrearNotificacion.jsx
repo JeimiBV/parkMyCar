4772a2b775fc5632f878fd8a3fe4f468e915f0b2
Adaluz Alvarez Santos <115201336+Adaluz-Alvarez-Santos@users.noreply.github.com>
2023-05-04 21:47:02 -0400

    Notificacion terminada

--- src/pages/CrearNotificacion.jsx
+++ src/pages/CrearNotificacion.jsx
@@ -1,21 +1,58 @@
 import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import "../styles/PagesStyles/CrearNotificacion.css";
+import reserves from "../../public/data/reservas.json";
 
 const CrearNotificacion = () => {
-  const options = [
-    { value: "Pepe", label: "68547228 " },
-    { value: "Juan", label: "75413878" },
-    { value: "Maria", label: "69712351" },
-  ];
+  const [reserves, setReserves] = useState([]);
+
+  useEffect(() => {
+    fetch("../../public/data/reservas.json")
+      .then((response) => response.json())
+      .then((data) => setReserves(data.reserves));
+  }, []);
+
+  const optionsMapped = reserves.map((reserve) => ({
+    value: reserve.phone,
+    label: `${reserve.phone} - ${reserve.plate}`,
+  }));
+
+  const [text, setText] = useState("");
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    setOptions(optionsMapped);
+  }, [reserves]);
+
+  const handleChange = (event) => {
+    let inputText = event.target.value;
+    inputText = inputText.replace(/[^a-zA-Z0-9]/g, "").substr(0, 150);
+    setText(inputText);
+  };
+  console.log(options);
+
+  const handleOption = (state) => {
+    setOptions(state);
+  };
 
   return (
+    
     <div className="contenedorNot">
       <div className="tituloNoti">Crear Notificación</div>
       <h3 className="tituloPara">Para: </h3>
 
       <div className="contSelect">
-        <Select className="opcionesNot" options={options} isMulti />
+        <Select
+          className="opcionesNot"
+          options={[{ label: "Select All", value: "all" }, ...optionsMapped]}
+          value = {options}
+          isMulti
+          onChange={(selected) => {
+            selected.length && selected.find((opt) => opt.value === "all")
+              ? setOptions(optionsMapped.slice(0))
+              : setOptions(selected);
+          }}
+        />
       </div>
 
       <div>
@@ -23,12 +60,19 @@ const CrearNotificacion = () => {
 
         <div className="contDes">
           <textarea
+            value={text}
             className="form-control Descripcion "
             placeholder="Escriba el mensaje"
             id="floatingTextarea2"
+            onChange={handleChange}
           ></textarea>
           <div className="m-2">
-            <button type="button" className="btn btn-primary botonDes" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <button
+              type="button"
+              className="btn btn-primary botonDes"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
               Enviar
             </button>
           </div>
@@ -47,7 +91,11 @@ const CrearNotificacion = () => {
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5 " id="staticBackdropLabel" style={{color:"black"}}>
+              <h1
+                class="modal-title fs-5 "
+                id="staticBackdropLabel"
+                style={{ color: "black" }}
+              >
                 Enviar Mensaje
               </h1>
               <button
@@ -66,7 +114,11 @@ const CrearNotificacion = () => {
               >
                 Cerrar
               </button>
-              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
                 Si, aceptar
               </button>
             </div>
