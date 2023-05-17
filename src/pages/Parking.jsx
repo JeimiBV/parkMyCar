eef88433123858ed8c72280ae6972a25f2fea559
unknown <david_calani@hotmail.com>
2023-05-17 11:46:41 -0400

    Filtered Search Added

--- src/pages/Parking.jsx
+++ src/pages/Parking.jsx
@@ -12,17 +12,12 @@ import "../styles/PagesStyles/Parqueo.css";
 
 function Parking() {
   const [places, setPlaces] = useState([]);
-  const [entrada, setEntrada] = useState("");
-  const [salida, setSalida] = useState("");
+  const [entryDate, setEntryDate] = useState("");
+  const [retirementDate, setRetirementDate] = useState("");
+  const [entryTime, setEntryTime] = useState("");
+  const [retirementTime, setRetirementTime] = useState("");
   const tableSection = splitIntoSection(places);
   const usuario = useSelector((state) => state.users).userState;
-  const handleEntradaChange = (event) => {
-    setEntrada(event.target.value);
-  };
-
-  const handleSalidaChange = (event) => {
-    setSalida(event.target.value);
-  };
 
   const getPlaces = async () => {
     const places = await fetchData();
@@ -39,6 +34,24 @@ function Parking() {
     setPlaces((places) => places.filter((place) => place.id !== id));
   };
 
+  const handleSearch = () => {
+    const availablePlaces = placesData.Places.filter((place) => {
+      // Check if there are any history entries for this place that overlap with the specified time range
+      const overlappingHistory = historyData.History.some((history) => {
+        return (
+          history.placeId === place.id &&
+          history.entryDate <= retirementDate &&
+          history.retirementDate >= entryDate &&
+          history.entryTime <= retirementTime &&
+          history.retirementTime >= entryTime
+        );
+      });
+      // Return true if there are no overlapping history entries for this place
+      return !overlappingHistory;
+    });
+    setPlaces(availablePlaces);
+  };
+
   useEffect(() => {
     getPlaces();
   }, []);
@@ -47,29 +60,52 @@ function Parking() {
     <div className="containerParqueo">
       <h1>Plazas</h1>
       <div className="buscadores">
-        <div className="buscador">
+        <div>
+          <label>Entry date:</label>
           <input
-            type="text"
-            value={entrada}
-            onChange={handleEntradaChange}
-            placeholder="Hora de entrada"
+            type="date"
+            value={entryDate}
+            onChange={(e) => setEntryDate(e.target.value)}
           />
         </div>
-        <div className="buscador">
+        <div>
+          <label>Retirement date:</label>
           <input
-            type="text"
-            value={salida}
-            onChange={handleSalidaChange}
-            placeholder="Hora de salida"
+            type="date"
+            value={retirementDate}
+            onChange={(e) => setRetirementDate(e.target.value)}
           />
         </div>
+        <div>
+          <label>Entry time:</label>
+          <input
+            type="time"
+            value={entryTime}
+            onChange={(e) => setEntryTime(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Retirement time:</label>
+          <input
+            type="time"
+            value={retirementTime}
+            onChange={(e) => setRetirementTime(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSearch}>Search</button>
       </div>
       <div className="tables-container">
         {tableSection.map((tableData, index) => (
           <ParkingSection key={index} data={tableData} />
         ))}
       </div>
-      <div className={usuario.rol=="admin"?"col-12 d-flex justify-content-end px-5":"d-none"}>
+      <div
+        className={
+          usuario.rol == "admin"
+            ? "col-12 d-flex justify-content-end px-5"
+            : "d-none"
+        }
+      >
         <button className="AddPlaceButton" onClick={() => CreatePlace()}>
           +
         </button>
