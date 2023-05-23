706245d21ae746a4715d1448d4f2dd168248e884
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2023-05-23 13:10:10 -0400

    csmbios

--- src/pages/InicioSesion.jsx
+++ src/pages/InicioSesion.jsx
@@ -3,11 +3,10 @@ import { useDispatch } from "react-redux";
 import { iniciarSesion } from "../users/userSlice";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { postPeticion, postAuthorization } from "../functions/useFetch";
 import jwt_decode from "jwt-decode";
 
 export default function InicioSesion() {
-  const [logInFail, setLoginFail]=useState(false);
+  const [logInFail, setLoginFail] = useState(false);
   const [showPwd, setShowPwd] = useState(false);
   const [data, setData] = useState({
     email: "",
@@ -53,58 +52,64 @@ export default function InicioSesion() {
       });
   };
   return (
-    <div className="containerInicio p-5">
-      <div class="wrapper">
-        <div class="h2I text-center fs-1 ">Iniciar sesión</div>
-        <form class="pt-3 d-block justify-content-center">
-          <div class="form-group py-2">
-            <p>Ingrese su correo</p>
-            <div class={logInFail?"input-field border-danger":"input-field "}>
-            <i class="fa-solid fa-user p-2"></i>
-              <input
-                name="email"
-                className="inputColor"
-                onChange={(e) => {
-                  handleChange(e);
-                }}
-              />
-            </div>
-          </div>
-          <div class="form-group py-1 pb-2">
-            <p>Ingrese su contraseña</p>
-            <div class={logInFail?"input-field border-danger":"input-field "}>
-              <span class="fas fa-lock p-2"></span>
-              <input
-                name="password"
-                type={showPwd ? "text" : "password"}
-                className="inputColor "
-                onChange={(e) => {
-                  handleChange(e);
-                }}
-              />
-              <div class="btn text-muted" onClick={() => setShowPwd(!showPwd)}>
-                {showPwd ? (
-                  <i class="fa-solid fa-eye eyecolor"></i>
-                ) : (
-                  <i class="fa-solid fa-eye-slash eyecolor"></i>
-                )}
+    <div className="containerInicio p-5 d-flex justify-content-center">
+      <div className="d-flex w-75">
+        <div class="welcomeb h-100 d-flex flex-column justify-content-center align-items-center p-5 text-center w-50">
+          <h1>Bienvenido de vuelta!</h1>
+          <p className="text-light">Para mantenerse conectado con nosotros, inicie sesión con su información personal</p>
+        </div>
+        <div class="wrapper p-5 pt-1 pb-1 h-100 overflow-y-scroll w-50 d-flex flex-column justify-content-center">
+          <div class="h2I text-center fs-1 ">Iniciar sesión</div>
+          <form class="pt-3 d-block justify-content-center">
+            <div class="form-group py-2">
+              <p>Ingrese su correo</p>
+              <div class={logInFail ? "input-field border-danger" : "input-field "}>
+                <i class="fa-solid fa-user p-2"></i>
+                <input
+                  name="email"
+                  className="inputColor w-100"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
               </div>
             </div>
-          </div>
-          <div className="w-100 text-center">
-            <p className={logInFail? "text-danger":"d-none"}>
-              Por favor verifique sus credenciales y vuelva a intentar
-            </p>
-            <button
-              class="btn btn-block text-center my-3 rounded btnInicio"
-              onClick={(event) => {
-                cambiarEstado(event);
-              }}
-            >
-              Ingresar
-            </button>
-          </div>
-        </form>
+            <div class="form-group py-1 pb-2">
+              <p>Ingrese su contraseña</p>
+              <div class={logInFail ? "input-field border-danger" : "input-field "}>
+                <span class="fas fa-lock p-2"></span>
+                <input
+                  name="password"
+                  type={showPwd ? "text" : "password"}
+                  className="inputColor w-100"
+                  onChange={(e) => {
+                    handleChange(e);
+                  }}
+                />
+                <div class="btn text-muted" onClick={() => setShowPwd(!showPwd)}>
+                  {showPwd ? (
+                    <i class="fa-solid fa-eye eyecolor"></i>
+                  ) : (
+                    <i class="fa-solid fa-eye-slash eyecolor"></i>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="w-100 text-center">
+              <p className={logInFail ? "text-danger" : "d-none"}>
+                Por favor verifique sus credenciales y vuelva a intentar
+              </p>
+              <button
+                class="btn btn-block text-center my-3 rounded btnInicio"
+                onClick={(event) => {
+                  cambiarEstado(event);
+                }}
+              >
+                Ingresar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/RegistroUsuario.jsx
+++ src/pages/RegistroUsuario.jsx
@@ -1,68 +1,100 @@
 import "../styles/PagesStyles/RegistroUsuario.css"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { postPeticion, postAuthorization } from "../functions/useFetch";
+import {toast} from "react-toastify";
 
 export default function RegistroUsuario() {
     const [showPwd, setShowPwd] = useState(false);
+    const [datosForm, setDatosForm] = useState({
+        name: "",
+        email: "",
+        password: "",
+        nit: "",
+        phone: 0,
+    });
     const navigate = useNavigate();
-    return (
-        <div className="containerRegistro p-4 overflow-y-scroll">
-            <form class="wrapperRegistro p-5 pt-1 pb-1">
-                <h2 className="mt-2 h2I text-center fs-1 mb-5">Registro de usuario</h2>
-                <div className="form-group mb-2 p-2 d-block ">
-                    <p className="w-100">Nombre</p>
-                    <div className=" groupRegistro w-100">
-                        <i class="fa-solid fa-user me-2 pe-2"></i>
-                        <input type="text" className="inputRegistro w-100" />
-                    </div>
-                </div>
-                <div className="form-group mb-2 p-2 d-block">
-                    <p className="w-100">Teléfono</p>
-                    <div className="w-75 groupRegistro w-100">
-                        <i class="fa-solid fa-phone me-2 pe-2"></i>
-                        <input className="inputRegistro w-100" />
-                    </div>
-                </div>
-                <div className="form-group mb-2 p-2 d-block">
-                    <p className="w-100">Carnet de identidad</p>
-                    <div className=" groupRegistro w-100">
-                        <i class="fa-regular fa-id-card me-2 pe-2"></i>
-                        <input className="inputRegistro w-100" />
-                    </div>
-                </div>
-                <div className="form-group mb-2 p-2 d-block">
-                    <p className="w-100">Vehículo</p>
-                    <div className="w-100">
-                        <div className=" groupRegistro w-100">
-                            <i class="fa-solid fa-car me-2 pe-2"></i>
-                            <input className="inputRegistro w-100" />
-                        </div>
-                    </div>
-                </div>
-                <div className="form-group mb-2 p-2 d-block">
-                    <p className="w-100">Correo electrónico</p>
-                    <div className=" groupRegistro w-100">
-                        <i class="fa-solid fa-at me-2 pe-2"></i>
-                        <input className="inputRegistro w-100" />
-                    </div>
-                </div>
-                <div className="form-group mb-2 p-2 d-block">
-                    <p className="w-100">Contraseña</p>
-                    <div className=" groupRegistro w-100">
-                        <i class="fa-solid fa-lock me-2 pe-2"></i>
-                        <input type={showPwd ? "text" : "password"} className="inputRegistro w-100" />
-                        <div class="me-0" onClick={() => setShowPwd(!showPwd)}>
-                            {showPwd ? <i class="fa-solid fa-eye eyecolor"></i> : <i class="fa-solid fa-eye-slash eyecolor"></i>}
-                        </div>
-                    </div>
-                </div>
-                <div className="w-100 row ms-1">
-                    <button class="btn col-4  btn-block text-center my-3 rounded" onClick={()=>{navigate("/")}}>Cancelar</button>
-                    <div className="col-4"></div>
 
-                    <button class="btn btn-block col-4 text-center my-3 rounded me-0">Crear cuenta</button>
+    const handleChange = (e) => {
+        setDatosForm({ ...datosForm, [e.target.name]: e.target.value });
+    };
+
+    const handlePost = async (e) => {
+        e.preventDefault();
+        /*await postPeticion(
+            "http://testingapi12023-001-site1.atempurl.com/users/client",
+            datosForm
+        );*/
+        toast.success('Registrando usuario', { theme: "colored", autoClose: 3000});
+        //navigate("/");
+    };
+
+    return (
+        <div className="containerRegistro p-5">
+            <div className="d-flex w-75">
+                <div class="welcome bg-danger h-100 d-flex flex-column justify-content-center align-items-center p-5 text-center w-50">
+                    <h1>Bienvenido!</h1>
+                    <p className="text-light">Ingresa tus datos personales y comienza a reservar con nosotros </p>
                 </div>
-            </form>
+                <form class="wrapperRegistro p-5 pt-1 pb-1 h-100 overflow-y-scroll w-50" onSubmit={(e) => handlePost(e)}>
+                    <h2 className="mt-2 h2I text-center fs-1 mb-4">Crear cuenta</h2>
+                    <div className="form-group p-2 d-block ">
+                        <p className="w-100 mb-1">Nombre</p>
+                        <div className=" groupRegistro w-100">
+                            <i class="fa-solid fa-user me-2 pe-2"></i>
+                            <input name="name" type="text" className="inputRegistro w-100" onChange={handleChange} required pattern="[a-zA-Z\s]+" />
+                        </div>
+                    </div>
+                    <div className="form-group p-2 d-block">
+                        <p className="w-100 mb-1">Teléfono</p>
+                        <div className="w-75 groupRegistro w-100">
+                            <i class="fa-solid fa-phone me-2 pe-2"></i>
+                            <input name="phone" className="inputRegistro w-100" onChange={handleChange} required pattern="[0-9]{8}" />
+                        </div>
+                    </div>
+                    <div className="form-group p-2 d-block">
+                        <p className="w-100 mb-1">Carnet de identidad</p>
+                        <div className=" groupRegistro w-100">
+                            <i class="fa-regular fa-id-card me-2 pe-2"></i>
+                            <input name="nit" className="inputRegistro w-100" onChange={handleChange} required pattern="[a-zA-Z0-9]+" />
+                        </div>
+                    </div>
+                    <div className="form-group p-2 d-block">
+                        <p className="w-100 mb-1">Vehículo</p>
+                        <div className="w-100">
+                            <div className=" groupRegistro w-100">
+                                <i class="fa-solid fa-car me-2 pe-2"></i>
+                                <input className="inputRegistro w-100" onChange={handleChange} required />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-group p-2 d-block">
+                        <p className="w-100 mb-1">Correo electrónico</p>
+                        <div className=" groupRegistro w-100">
+                            <i class="fa-solid fa-at me-2 pe-2"></i>
+                            <input type="email" name="email" className="inputRegistro w-100" onChange={handleChange} required />
+                        </div>
+                    </div>
+                    <div className="form-group p-2 d-block">
+                        <p className="w-100 mb-1">Contraseña</p>
+                        <div className=" groupRegistro w-100">
+                            <i class="fa-solid fa-lock me-2 pe-2"></i>
+                            <input name="password" type={showPwd ? "text" : "password"} className="inputRegistro w-100" onChange={handleChange} required />
+                            <div class="me-0" onClick={() => setShowPwd(!showPwd)}>
+                                {showPwd ? <i class="fa-solid fa-eye eyecolor"></i> : <i class="fa-solid fa-eye-slash eyecolor"></i>}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-100 row ms-1">
+                        <button class="btn col-4  btn-block text-center my-3 rounded" onClick={() => { navigate("/") }}>Cancelar</button>
+                        <div className="col-4"></div>
+
+                        <button data-bs-toggle="modal" type="submit" class="btn btn-block col-4 text-center my-3 rounded me-0">Crear cuenta</button>
+                    </div>
+                </form>
+
+                
+            </div>
         </div>
     );
 }
