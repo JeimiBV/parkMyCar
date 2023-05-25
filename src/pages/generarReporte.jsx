3e91bff5c2c1e7357f5892f7ba4421bc7deaf54f
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2023-05-25 10:08:38 -0400

    jueves

--- src/pages/generarReporte.jsx
+++ src/pages/generarReporte.jsx
@@ -8,15 +8,15 @@ export default function () {
     const [placa, setPlaca] = useState("");
     const [datae, setData] = useState();
     const [active, setActive] = useState(false);
- console.log(datae, "datpopos")
-    const handlePlate=()=>{
-       fetch(`http://testingapi12023-001-site1.atempurl.com/Reserves/Plate/${placa}`)
-        .then((response) => response.json())
-        .then((datos) => {
-        setData(datos)
-        console.log(datae)
-        })
-       
+    console.log(datae, "datpopos")
+    const handlePlate = () => {
+        fetch(`http://testingapi12023-001-site1.atempurl.com/Reserves/Plate/${placa}`)
+            .then((response) => response.json())
+            .then((datos) => {
+                setData(datos)
+                console.log(datos)
+            })
+
 
     }
 
@@ -25,25 +25,25 @@ export default function () {
             <h1>Generar Reporte</h1>
             <div className="cardReporte p-5 rounded h-100 mb-3">
                 <div className="row w-100 me-0 ms-0 mb-5">
-                   
-                    
-                   
+
+
+
                     <div className="col d-flex justify-content-center row">
-                    <input type="text" placeholder="Buscar placa" id="buscadorPlaca" className="w-25 me-3" name="placa" onChange={e =>{setPlaca(e.target.value)}} />
-                        <button type="button" class="btn botonReporte w-25" onClick={()=>{handlePlate()}}>
+                        <input type="text" placeholder="Buscar placa" id="buscadorPlaca" className="w-25 me-3" name="placa" onChange={e => { setPlaca(e.target.value) }} />
+                        <button type="button" class="btn botonReporte w-25" onClick={() => { handlePlate() }}>
                             <i class="fa-solid fa-magnifying-glass me-2"></i>
                             Buscar
                         </button>
                     </div>
                 </div>
-                {  datae?
+                {datae ?
                     <PDFViewer style={{ width: "100%", height: "80%" }}>
-                    <DocumentPDF
-                    datae={datae}
-                    ></DocumentPDF>
-                </PDFViewer>:<>
-                    Ingrese la placa del vehiculo del cual quiere hacer seguimiento
-                </>
+                        <DocumentPDF
+                            datae={datae}
+                        ></DocumentPDF>
+                    </PDFViewer> : <>
+                        Ingrese la placa del vehiculo del cual quiere hacer seguimiento
+                    </>
                 }
 
             </div>
