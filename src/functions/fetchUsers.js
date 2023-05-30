d4f71421668fa93912c22c0737b388fd909ec69a
unknown <david_calani@hotmail.com>
2023-05-30 15:04:25 -0400

    Added Edit User

--- src/functions/fetchUsers.js
+++ src/functions/fetchUsers.js
@@ -4,3 +4,24 @@ export const fetchData = async () => {
   );
   return await response.json();
 };
+
+export const EditData = async (id, newName, newNit, newPhone, newEmail) => {
+  const userData = {
+    name: newName,
+    nit: newNit,
+    phone: newPhone,
+    email: newEmail,
+  };
+  const response = await fetch(
+    `http://testingapi12023-001-site1.atempurl.com/users/${id}`,
+    { method: "PUT", body: JSON.stringify(userData) }
+  );
+  return await response.json();
+};
+
+export const DeleteData = async () => {
+  const response = await fetch(
+    "http://testingapi12023-001-site1.atempurl.com/users"
+  );
+  return await response.json();
+};
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "../styles/PagesStyles/AdministrarInfo.css";
 import { Navigate } from "react-router-dom";
+import { EditData } from "../functions/fetchUsers";
 
 const User = ({ usuario }) => {
   const [name, setName] = useState(usuario.name);
+  const [nit, setNit] = useState(usuario.nit);
+  const [phone, setPhone] = useState(usuario.phone);
+  const [email, setEmail] = useState(usuario.email);
+
   const [isEditing, SetIsEditing] = useState(false);
-  const editarUsuario = (name) => {
-    console.log("Editando usuario:", nombre);
+
+  const editarUsuario = async () => {
+    await EditData(name, nit, phone, email);
+    SetIsEditing(false);
   };
+
   const eliminarUsuario = (nombre) => {
     console.log("Eliminando usuario:", nombre);
   };
+
   return (
     <div key={usuario.nombre} className="usuarioFinal">
       <div className="usuarioUnidoID">
@@ -27,7 +36,12 @@ const User = ({ usuario }) => {
               }}
             >
               <p>Nombre: </p>
-              <input disabled={!isEditing} type="text" value={name} />
+              <input
+                disabled={!isEditing}
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div
               style={{
@@ -37,7 +51,12 @@ const User = ({ usuario }) => {
               }}
             >
               <p>CI: </p>
-              <input disabled type="text" value={usuario.nit} />
+              <input
+                disabled={!isEditing}
+                type="text"
+                value={nit}
+                onChange={(e) => setNit(e.target.value)}
+              />
             </div>
             <div
               style={{
@@ -47,7 +66,12 @@ const User = ({ usuario }) => {
               }}
             >
               <p>Teléfono: </p>
-              <input disabled type="text" value={usuario.phone} />
+              <input
+                disabled={!isEditing}
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </div>
             <div
               style={{
@@ -57,18 +81,33 @@ const User = ({ usuario }) => {
               }}
             >
               <p>Correo: </p>
-              <input disabled type="text" value={usuario.email} />
+              <input
+                disabled={!isEditing}
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
           </div>
         </div>
       </div>
       <div className="botonUsuario">
-        <button
-          class="btn btn-primary m-2"
-          onClick={() => editarUsuario(usuario.nombre)}
-        >
-          Editar
-        </button>
+        {!isEditing ? (
+          <button
+            class="btn btn-primary m-2"
+            onClick={() => SetIsEditing(true)}
+          >
+            Editar
+          </button>
+        ) : (
+          <button
+            class="btn btn-primary m-2"
+            onClick={() => editarUsuario(usuario.nombre)}
+          >
+            Finalizar
+          </button>
+        )}
+
         <button
           class="btn btn-primary m-2"
           onClick={() => eliminarUsuario(usuario.nombre)}
