4b90c401efa8ae10a7e0cc02c3ffcdb01b375e8d
JeimiBV <115201224+JeimiBV@users.noreply.github.com>
2023-05-31 22:18:36 -0400

    cambio de fecha

--- src/pages/Parking.jsx
+++ src/pages/Parking.jsx
@@ -78,6 +78,11 @@ function Parking() {
     getPlaces();
     getReserves();
   }, [entryTime, entryDate]);
+  
+  const modificarDate = (currentDate) => {
+    return `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}T${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}.${currentDate.getMilliseconds().toString().padStart(3, '0')}Z`
+  }
+
 
   return (
     <div className="containerParqueo overflow-y-scroll p-3">
@@ -88,7 +93,7 @@ function Parking() {
           <input
             className="h-100 buscador p-1"
             type="date"
-            min={new Date().toISOString().slice(0, 10)}
+            min={modificarDate(new Date()).slice(0,10)}
             value={entryDate}
             onChange={(e) => setEntryDate(e.target.value)}
           />
