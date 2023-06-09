37693daabf3a79f7b5447c97855308b9421bbfb7
unknown <david_calani@hotmail.com>
2023-06-08 23:12:00 -0400

    Added validation

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -22,134 +22,155 @@ const User = ({ usuario, deleteUser }) => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  const editarUsuario = async () => {
-    await EditData(usuario.id, name, nit, phone, email, vehicles);
+  const confirmUser = async () => {
     SetIsEditing(false);
+    await EditData(usuario.id, name, nit, phone, email, vehicles);
   };
 
+  const editUser = (e) => {
+    e.preventDefault();
+    SetIsEditing(true);
+  };
+
+  console.log(isEditing);
   return (
     <div key={usuario.nombre} className="usuarioFinal p-2 m-3">
-      <div className="usuarioUnidoID m-3">
-        <div className="usuarioIzq">
-          <i class="fa-solid fa-user fs-1 me-3 opciones iconoUser"></i>
-        </div>
-        <div className="usuarioDer">
-          <div className="datosUsuario">
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              className="row"
-            >
-              <p className="datosUser col-4">Nombre: </p>
-              <input
-                className="col"
-                disabled={!isEditing}
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              className="row"
-            >
-              <p className="datosUser col-4">CI: </p>
-              <input
-                className="col"
-                disabled={!isEditing}
-                type="text"
-                value={nit}
-                onChange={(e) => setNit(e.target.value)}
-              />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              className="row"
-            >
-              <p className="datosUser col-4">Teléfono: </p>
-              <input
-                className="col"
-                disabled={!isEditing}
-                type="text"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </div>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              className="row"
-            >
-              <p className="datosUser col-4">Correo: </p>
-              <input
-                className="col"
-                disabled={!isEditing}
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            {usuario.role === "Client" ? (
-              <div className="row p-1 w-100 bg-warning ms-2">
-                <div className=" w-100  p-1 text-center" onClick={toggleDropdown}>
-                  desplegar lista de placas
-                </div>
-                {isDropdownOpen && (
-                  <ul className="list-unstyled col-8">
-                    {vehicles.map((vehicle, index) => (
-                      <li key={vehicle.id}>
-                        <input
-                          className=""
-                          type="text"
-                          disabled={!isEditing}
-                          value={vehicle.plate}
-                          onChange={(event) =>
-                            handlePlateChange(event, index)
-                          }
-                        />
-                      </li>
-                    ))}
-                  </ul>
-                )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          confirmUser();
+        }}
+      >
+        <div className="usuarioUnidoID m-3">
+          <div className="usuarioIzq">
+            <i class="fa-solid fa-user fs-1 me-3 opciones iconoUser"></i>
+          </div>
+          <div className="usuarioDer">
+            <div className="datosUsuario">
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+                className="row"
+              >
+                <p className="datosUser col-4">Nombre: </p>
+                <input
+                  className="col"
+                  disabled={!isEditing}
+                  type="text"
+                  required
+                  minLength={6}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
-            ) : null}
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+                className="row"
+              >
+                <p className="datosUser col-4">CI: </p>
+                <input
+                  className="col"
+                  disabled={!isEditing}
+                  type="text"
+                  required
+                  minLength={6}
+                  value={nit}
+                  onChange={(e) => setNit(e.target.value)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+                className="row"
+              >
+                <p className="datosUser col-4">Teléfono: </p>
+                <input
+                  className="col"
+                  disabled={!isEditing}
+                  type="text"
+                  required
+                  minLength={7}
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+                className="row"
+              >
+                <p className="datosUser col-4">Correo: </p>
+                <input
+                  className="col"
+                  disabled={!isEditing}
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              {usuario.role === "Client" ? (
+                <div className="row p-1 w-100 bg-warning ms-2">
+                  <div
+                    className=" w-100  p-1 text-center"
+                    onClick={toggleDropdown}
+                  >
+                    desplegar lista de placas
+                  </div>
+                  {isDropdownOpen && (
+                    <ul className="list-unstyled col-8">
+                      {vehicles.map((vehicle, index) => (
+                        <li key={vehicle.id}>
+                          <input
+                            className=""
+                            type="text"
+                            disabled={!isEditing}
+                            value={vehicle.plate}
+                            onChange={(event) =>
+                              handlePlateChange(event, index)
+                            }
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="botonUsuario">
-        {!isEditing ? (
-          <button
-            class="btn btn-primary m-2 botonUser"
-            onClick={() => SetIsEditing(true)}
-          >
-            Editar
-          </button>
-        ) : (
-          <button
-            class="btn btn-success m-2 botonUser"
-            onClick={() => editarUsuario()}
-          >
-            Finalizar
-          </button>
-        )}
+        <div className="botonUsuario">
+          {!isEditing ? (
+            <button
+              type="button"
+              class="btn btn-primary m-2 botonUser"
+              onClick={(e) => editUser(e)}
+            >
+              Editar
+            </button>
+          ) : (
+            <button type="submit" class="btn btn-success m-2 botonUser">
+              Finalizar
+            </button>
+          )}
 
-        <button
-          class="btn btn-danger m-2 botonUser"
-          onClick={() => deleteUser()}
-        >
-          Eliminar
-        </button>
-      </div>
+          <button
+            class="btn btn-danger m-2 botonUser"
+            onClick={() => deleteUser()}
+          >
+            Eliminar
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
