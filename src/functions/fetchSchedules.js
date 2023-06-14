7e3b98bf84c6a9a2aa6ffcb3a56ce5eedf87dfbe
unknown <david_calani@hotmail.com>
2023-06-13 23:37:29 -0400

    Fixed Schedules

--- src/functions/fetchSchedules.js
+++ src/functions/fetchSchedules.js
@@ -12,18 +12,43 @@ export const fetchGetScheduleToday = async () => {
   return await response.json();
 };
 
-
-
 export const fetchPostData = async (schedules) => {
+  const modificarDate = (currentDate) => {
+    return `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${currentDate
+      .getDate()
+      .toString()
+      .padStart(2, "0")}T${currentDate
+      .getHours()
+      .toString()
+      .padStart(2, "0")}:${currentDate
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}:${currentDate
+      .getSeconds()
+      .toString()
+      .padStart(2, "0")}.${currentDate
+      .getMilliseconds()
+      .toString()
+      .padStart(3, "0")}Z`;
+  };
+  const updatedData = schedules.map((item) => {
+    item.startDate = modificarDate(item.startDate); // Convert start date to ISO string format
+    item.endDate = modificarDate(item.endDate); // Convert end date to ISO string format
+
+    return item;
+  });
+
   const response = await fetch(
     "http://parkmycar-001-site1.atempurl.com/schedules",
     {
       method: "POST",
-      body: JSON.stringify(schedules),
+      body: JSON.stringify(updatedData),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
     }
   );
-  return await response.json();
+  return response.json();
 };
